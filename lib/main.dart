8e05678a29395c09514a9d818d0c16828ae617be
equalcoding <info@equalcoding.dev>
2021-05-22 15:22:07 +0100

    Column And Row Layout

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext ctx) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('AppBar Title'),
-        ),
-        body: Text('Example Widget'),
-      ),
+          appBar: AppBar(
+            title: Text('AppBar Title'),
+          ),
+          body: Row(
+            children: [
+              Column(
+                children: [
+                  Text('ROW1COL1,'),
+                  Text('ROW1COL2,'),
+                  ElevatedButton(
+                    child: Text('Button Text'),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  Text('ROW2COL1,'),
+                  Text('ROW2COL2,'),
+                  ElevatedButton(
+                    onPressed: () => {},
+                    child: Text('Button Text'),
+                  ),
+                ],
+              ),
+            ],
+          )),
     );
   }
 }
